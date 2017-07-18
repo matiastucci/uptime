958e0f63f2b22e38479991afd5c74d176f8861e2
Matias Tucci <matiastucci@gmail.com>
2017-07-18 00:30:45 -0300

    use live api

--- client/src/services/config/index.js
+++ client/src/services/config/index.js
@@ -3,6 +3,7 @@ const config = {}
 config.MAX_ELEMENTS = 60
 config.MAX_AVERAGE = 1
 config.MESSAGES_INTERVAL = 2 * 60 * 1000
-config.BASE_URL = 'http://localhost:3000'
+// config.BASE_URL = 'http://localhost:3000'
+config.BASE_URL = 'https://uptime-server.now.sh'
 
 export default config
